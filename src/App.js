5fab12ae8d186f397bc665bbea113dd1c4378dd4
mikivujkovic <vujkovic@gmail.com>
2019-05-10 11:50:48 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Link } from "@reach/router";
+import Swim from "./components/Swim";
+import T1 from "./components/T1";
+import T2 from "./components/T2";
+import Finish from "./components/Finish";
+import Bike from "./components/Bike";
+import Km5 from "./components/Km5";
+import Km10 from "./components/Km10";
+import Km15 from "./components/Km15";
+import Km27 from "./components/Km27";
+import Km45 from "./components/Km45";
+import Km63 from "./components/Km63";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class PersonList extends React.Component {
+  
+  render() {
+    return (
+     <div>
+      <nav className="nav">
+        <Link to="swim">Swim </Link>{"   "}
+        <Link to="t1">T1 </Link>{"   "}
+        <Link to="km27">27km </Link>{"   "}
+        <Link to="km45">45km </Link>{"   "}
+        <Link to="km63">63km </Link>{"   "}
+        <Link to="bike">Bike </Link>{"   "}
+        <Link to="t2">T2 </Link>{"   "}
+        <Link to="km5">5km </Link>{"   "}
+        <Link to="km10">10km </Link>{"   "}
+        <Link to="km15">15km </Link>{"   "}
+        <Link to="finish">Finish </Link>{"   "}
+      </nav>
+      <br />
+      <Router>
+        <Swim path="swim" />
+        <T1 path="t1" />
+        <Km27 path="km27"/>
+        <Km45 path="km45"/>
+        <Km63 path="km63"/>
+        <Bike path="bike"/>
+        <T2 path="t2"/>
+        <Km5 path="km5"/>
+        <Km10 path="km10"/>
+        <Km15 path="km15"/>
+        <Finish path="finish"/>
+    </Router>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Finish.js
@@ -0,0 +1,106 @@
+import React from "react";
+import axios from "axios";
+import "../styles.css";
+
+const desetinke = x => {
+  const sekunda = parseInt(x.split(":")[2], 10);
+  const minuta = parseInt(x.split(":")[1], 10);
+  const sat = parseInt(x.split(":")[0], 10);
+  return sekunda*10 + minuta*600 + sat*36000
+}
+
+export default class Finish extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rezultatiZ: [],
+      rezultatiM: []
+    };
+  }
+  
+  componentDidMount() {
+
+    const jeMusko = x => {
+      return x[5] === "Male"
+    }
+
+    const jeZensko = x => {
+      return x[5] === "Female"
+    }
+
+    
+
+    const sortiranje = (a, b) => {
+      return desetinke(a[16]) - desetinke(b[16])
+    }
+
+    axios.get(`https://api.raceresult.com/123664/YT3QE5CKGWU6Y6C4VD33BLSCEK2C2ZEX`).then(res => {
+      const rezultatiZ = res.data.data.filter(jeZensko).sort(sortiranje).slice(0, 5);
+      const rezultatiM = res.data.data.filter(jeMusko).sort(sortiranje).slice(0, 5);
+      this.setState({ rezultatiZ, rezultatiM });
+    });
+  }
+
+  jeMusko = x => {
+    return x[5] === "M";
+  };
+
+  jeZensko = x => {
+    return x[5] === "Ž";
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <div className="naslov">Female Finish</div>
+          <table className="table">
+            <thead>
+              <tr className="tableHeader">
+                <th className="header-100">Bib</th>
+                <th className="header-800">Name</th>
+                <th className="header-100">Country</th>
+                <th className="header-100">Gun Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.rezultatiZ.map((result, i) => (
+                <tr key={i}>
+                  <td>{result[0]}</td>
+                  <td>{result[2]}</td>
+                  <td>{result[3]}</td>
+                  <td>{result[16]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+        <div className="naslov">Male Finish</div>
+          <table className="table">
+            <thead>
+              <tr className="tableHeader">
+                <th className="header-100">Bib</th>
+                <th className="header-800">Name</th>
+                <th className="header-100">Country</th>
+                <th className="header-100">Gun Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.rezultatiM.map((result, i) => (
+                <tr key={i}>
+                  <td>{result[0]}</td>
+                  <td>{result[2]}</td>
+                  <td>{result[3]}</td>
+                  <td>{result[16]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+    </div>
+    );
+  }
+}
